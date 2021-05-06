d5e64b4fc2681dfcc8e9a7ef122a992fa7ff5162
Rabindra Laheri <rabindra.laheri@mindtree.com>
2021-05-06 23:47:56 +0530

    Initial Commit

---
+++ Assignment2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment2
+//
+//  Created by M1029313 on 06/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
